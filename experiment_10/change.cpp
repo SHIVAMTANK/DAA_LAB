ff29b08c275ed1c31d79f8a0e0d58bbc100f4c24
SHIVAMTANK <shivamtank8059@gmail.com>
2024-03-12 12:59:27 +0530

    added

--- experiment_10/change.cpp
+++ experiment_10/change.cpp
@@ -1,29 +1,49 @@
-#include<time.h>
 #include <bits/stdc++.h>
 using namespace std;
-int count(int coins[], int n, int sum)
+
+int coins[] = {1,2,3,4,5}; 
+int dp [1000] = {0};
+int minCoins(int N, int M)
 {
-	if (sum == 0)
-		return 1;
-	if (sum < 0)
-		return 0;
-	if (n <= 0)
-		return 0;	
-	return count(coins, n, sum - coins[n - 1])
-		+ count(coins, n - 1, sum);
+  //Initializing all values to INT_MAX i.e. minimum coins to make any
+  //amount of sum is INT_MAX
+  for(int i = 0;i<=N;i++)
+    dp[i] = INT_MAX;
+  
+  //Base case 
+  //Minimum coins to make sum = 0 cents is 0
+  dp[0] = 0;
+  
+  //Iterating in the outer loop for possible values of sum between 1 to N
+  //Since our final solution for sum = N might depend upon any of these values
+  for(int i = 1;i<=N;i++)
+  {
+    //Inner loop denotes the index of coin array.
+    //For each value of sum, to obtain the optimal solution.
+    for(int j = 0;j<M;j++)
+    {
+      //i —> sum
+      //j —> next coin index
+      //If we can include this coin in our solution
+      if(coins[j] <= i)
+      {
+        //Solution might include the newly included coin
+        dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+      }
+    }
+  }
+  return dp[N];
 }
 
-int main()
-{
-	int i, j;
-	int coins[] = { 1, 2, 3 };
-	int n = sizeof(coins) / sizeof(coins[0]);
-	int sum = 5;
+int main() {
+  // your code goes here
+  int sum = 50;
+  int total_coins = 5;
     clock_t t = clock();
- 	int ans = count(coins,n,sum);
+ 	bool ans = minCoins(sum, total_coins);
     double time_taken = ((double)t)/CLOCKS_PER_SEC;
     cout<<time_taken;
     cout<<ans;
-	cout << " " <<ans;
-	return 0;
+  
+ 
 }
